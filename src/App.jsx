031f6f2b4f4c0127143d76363a19a239ce644dbb
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-02-06 12:59:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>HEllo its my portfolio</h1>
+      <button className='btn btn-primary'>Click</button>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
