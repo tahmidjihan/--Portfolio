6a947cb24a20db1acba17c3327512d74f6d5eb6f
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-02-08 14:58:32 +0600

    remade 404

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='*' element={<Error />} />
+        <Route path='/*' element={<Error />} />
         <Route path='/' element={<App />} />
         <Route path='/project/:id' element={<Project />} />
       </Routes>
